bc222d70c8235a769f5f1e576618f662ba7af8b1
ashhoveyan <ash.hoveyan.02@mail.ru>
2022-08-31 00:58:30 +0400

    Garoi hamar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import {HeaderComponent} from "./main/header/header.component";
 import {FooterComponent} from "./main/footer/footer.component";
 
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
+
 import { WorkDetailComponent } from './work-detail/work-detail.component';
-=======
+
 import {HttpClientModule} from "@angular/common/http";
->>>>>>> c8d09a265c291305ce04bbdee841e6a138f618bf
+import { BlogComponent } from './blog/blog.component';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {HttpClientModule} from "@angular/common/http";
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    WorkDetailComponent
+    WorkDetailComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
